406ff59280204da10be58c3e9dc565a873203303
Hasan Balbaki <hsnblbki@gmail.com>
2021-12-21 23:17:17 +0100

    most sold

--- src/index.js
+++ src/index.js
@@ -8,4 +8,8 @@ import '@fortawesome/fontawesome-free/js/all.min.js';
 
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
+
+    $(".add-to-card-btn").click(function(){
+        alert('أضف المنتج الى عربة الشراء');
+    });
   });
